ef556289e8405f5c68762d31c7389789d86adcfd
e-s2055 <elizabeth.b.s@protonmail.com>
2025-03-12 12:20:26 -0500

    Converted static path to sys

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+import sys
 from stats import get_num_words
+from stats import get_chars
+from stats import sort_on
 
 def get_book_text(file):
     with open(file) as f:
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else: 
+        book_path = sys.argv[1]
     text = get_book_text(book_path)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
 
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
 
+
+    char_counts = get_chars(text)
+    sorted_char_counts = sort_on(char_counts)
+    print("--------- Character Count -------")
+    for char_entry in sorted_char_counts:
+        print(f"{char_entry['letter']}: {char_entry['count']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,18 @@ def get_num_words(text):
     words = text.split()
     num_words = len(words)
     return num_words
+
+def get_chars(text):
+    chars = {}  # Empty dictionary to store counts
+    for letter in text.lower():
+        if letter.isalpha():
+            if letter in chars:
+                chars[letter] += 1
+            else: 
+                chars[letter] = 1
+    return chars
+
+def sort_on(chars):
+    char_list = [{"letter": letter, "count": count} for letter, count in chars.items()]
+    char_list.sort(reverse=True, key=lambda char: char["count"])
+    return char_list
